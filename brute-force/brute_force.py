1cf5e25de1dbd97d956e4cc3b3441e598c2fdd95
NTT-TNN <nguyentienthao96@gmail.com>
2017-12-15 13:29:12 +0700

    brute-force

--- brute-force/brute_force.py
+++ brute-force/brute_force.py
@@ -2,12 +2,6 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-for x in range(0,999,1)
-    if (x % 100) ==0
-        path= "/image.orig" + x +"jpg"
-        img = cv2.imread(path,0)
-
-
 img1 = cv2.imread('full.jpg',0)          # queryImage
 img2 = cv2.imread('full_img.jpg',0) # trainImage
 
@@ -17,10 +11,6 @@ surf = cv2.xfeatures2d.SURF_create()
 # find the keypoints and descriptors with SIFT
 kp1, des1 = surf.detectAndCompute(img1,None)
 kp2, des2 = surf.detectAndCompute(img2,None)
-np.savetxt('kp1.txt', kp1, delimiter=" ", fmt="%s")
-np.savetxt('des1.txt', des1, delimiter=" ", fmt="%s")
-np.savetxt('kp2.txt', kp2, delimiter=" ", fmt="%s")
-np.savetxt('des2.txt', des2, delimiter=" ", fmt="%s")
 # FLANN parameters
 FLANN_INDEX_KDTREE = 0
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
