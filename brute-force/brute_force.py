5e9d1e0ef4aedd97e92158e9002128e0613ece38
NTT-TNN <nguyentienthao96@gmail.com>
2017-12-15 13:48:46 +0700

    train data

--- brute-force/brute_force.py
+++ brute-force/brute_force.py
@@ -1,38 +1,20 @@
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
-
-img1 = cv2.imread('full.jpg',0)          # queryImage
-img2 = cv2.imread('full_img.jpg',0) # trainImage
-
-# Initiate SIFT detector
+# Initiate SURF detector
 surf = cv2.xfeatures2d.SURF_create()
+img_query = cv2.imread('image.orig/0.jpg',0)          # queryImage
+kp2, des2 = surf.detectAndCompute(img_query,None)
 
-# find the keypoints and descriptors with SIFT
-kp1, des1 = surf.detectAndCompute(img1,None)
-kp2, des2 = surf.detectAndCompute(img2,None)
-# FLANN parameters
-FLANN_INDEX_KDTREE = 0
-index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-search_params = dict(checks=50)   # or pass empty dictionary
+for x in range(0,999,1):
+    if (x % 100) !=0:
+        path_img ="image.orig/" + str(x) +".jpg"
+        print (path_img)
+        path_kp = "keypoint/" + str(x) + ".txt"
+        path_des = "des/" + str(x) + ".txt"
+        img = cv2.imread(path_img, 0)  # trainImage
+        # find the keypoints and descriptors with SURF
+        kp, des = surf.detectAndCompute(img, None)
+        np.savetxt(path_kp, kp, delimiter=" ", fmt="%s")
+        np.savetxt(path_des, des, delimiter=" ", fmt="%s")
 
-flann = cv2.FlannBasedMatcher(index_params,search_params)
-
-matches = flann.knnMatch(des1,des2,k=2)
-
-# Need to draw only good matches, so create a mask
-matchesMask = [[0,0] for i in range(len(matches))]
-
-# ratio test as per Lowe's paper
-for i,(m,n) in enumerate(matches):
-    if m.distance < 0.7*n.distance:
-        matchesMask[i]=[1,0]
-
-draw_params = dict(matchColor = (0,255,0),
-                   singlePointColor = (255,0,0),
-                   matchesMask = matchesMask,
-                   flags = 0)
-
-img3 = cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-
-plt.imshow(img3,),plt.show()
