aa7951320f3572afeda96ede7dfa539a08c52e77
NTT-TNN <nguyentienthao96@gmail.com>
2017-12-20 12:15:28 +0700

    web image sorting

---
+++ ImageSearch/searchApp/static/js/sorting.js
@@ -0,0 +1,29 @@
+/**
+ * Created by thao-nt on 20/12/2017.
+ */
+$(function () {
+  $(".js-upload-photos").click(function () {
+    $("#fileupload").click();
+  });
+
+  $("#fileupload").fileupload({
+    dataType: 'json',
+    done: function (e, data) {
+      console.log(data.result.result)
+         console.log("succeess");
+        $("#photo_file_url").attr("src",data.result.photo_file_url)
+         $("#results").empty();
+         for (var i=0;i<data.result.result.length;++i){
+            var figure=document.createElement("figure");
+            var image=document.createElement("IMG");
+            image.src="/static/"+data.result.result[i];
+            var figcaption=document.createElement("figcaption");
+            figcaption.append("/static/images/"+data.result.result[i]);
+            figure.appendChild(image);
+            figure.appendChild(figcaption);
+            $("#results").append(figure);
+         }
+      }
+  });
+
+});
